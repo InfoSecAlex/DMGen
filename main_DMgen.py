6f1854ca0400b9d5e96df40d4017afceab140507
InfoSecAlex <alexbradley.infosec@gmail.com>
2018-01-19 17:20:52 -0700

    1.0.1

--- main_DMgen.py
+++ main_DMgen.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
 # Created by Alex Bradley: alexbradley.infosec@gmail.com
 # Main file of the DMGen program. Revision: 1.0
+
+import sys, argparse, random
+
+parser = argparse.ArgumentParser()
+
+
